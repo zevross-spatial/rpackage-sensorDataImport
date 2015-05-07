6ca8389be91d9d09bf235db6bbd7a00110d6a3e9
zross <zev@zevross.com>
2015-05-07 12:23:39 -0400

    initial commit2

--- R/bikeData.R
+++ R/bikeData.R
@@ -3,13 +3,32 @@
 }
 
 
-createDatabase<-function{
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.devtools <- list(
+    devtools.path = "~/R-dev",
+    devtools.install.args = "",
+    devtools.name = "Your name goes here",
+    devtools.desc.author = '"First Last <first.last@example.com> [aut, cre]"',
+    devtools.desc.license = "What license is it under?",
+    devtools.desc.suggests = NULL,
+    devtools.desc = list()
+  )
+  toset <- !(names(op.devtools) %in% names(op))
+  if(any(toset)) options(op.devtools[toset])
+  
+  invisible()
+}
+
+
+
+createDatabase<-function(){
   
   
 }
 
 
-addTables<-function{
+addTables<-function(){
   
   
 }
