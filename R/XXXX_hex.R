29797d0143b89c6ee6cba8091b187e2b26d1db23
Zev Ross <zev@zevross.com>
2017-04-27 14:17:00 -0400

    minor

--- R/XXXX_hex.R
+++ R/XXXX_hex.R
@@ -34,34 +34,51 @@ hex_extract_convert <- function(thefile){
   info <- jsonlite::fromJSON(paste0(tmpdir, "/info.json")) 
   # info$start_date: "2016-09-15T17:24:47.003906"
   # format(as.POSIXct((info$timestamp), origin = "1970-01-01"), usetz=FALSE)
-  startdate <- as.POSIXct(gsub('T' , '', info$start_date), origin = "1970-01-01")
+  startdate <- as.POSIXct(gsub('T' , '', info$start_date), origin = "1970-01-01", tz = "GMT")
+  startdate <- as.POSIXct(format(startdate, tz = "America/New_York"))
   
   acceleration_X <- read.csv(paste0(tmpdir, "/acceleration_X.csv"), as.is = TRUE, col.names = c("second", "acceleration_x")) 
   acceleration_Y <- read.csv(paste0(tmpdir, "/acceleration_Y.csv"), as.is = TRUE, col.names = c("second", "acceleration_y"))
   acceleration_Z <- read.csv(paste0(tmpdir, "/acceleration_Z.csv"), as.is = TRUE, col.names = c("second", "acceleration_z")) 
   
-  avgToSecond(acceleration_X)
-  
+  acceleration_X <- avgToSecond(acceleration_X)
+  acceleration_Y <- avgToSecond(acceleration_Y)
+  acceleration_Z <- avgToSecond(acceleration_Z)
   
   activity <- read.csv(paste0(tmpdir, "/activity.csv"), as.is = TRUE, col.names = c("second", "activity")) 
   breathing_rate <- read.csv(paste0(tmpdir, "/breathing_rate.csv"), as.is = TRUE, col.names = c("second", "breathing_rate")) 
   cadence <- read.csv(paste0(tmpdir, "/cadence.csv"), as.is = TRUE, col.names = c("second", "cadence"))
   device_position <- read.csv(paste0(tmpdir, "/device_position.csv"), as.is = TRUE, col.names = c("second", "device_position")) 
+  device_position <- device_position[device_position$second!=0,]
+  
   heart_rate <- read.csv(paste0(tmpdir, "/heart_rate.csv"), as.is = TRUE, col.names = c("second", "heart_rate")) 
   minute_ventilation_adjusted <- read.csv(paste0(tmpdir, "/minute_ventilation_adjusted.csv"), as.is = TRUE, col.names = c("second", "minute_ventilation_adjusted")) 
   minute_ventilation_raw<- read.csv(paste0(tmpdir, "/minute_ventilation_raw.csv"), as.is = TRUE, col.names = c("second", "minute_ventilation_raw")) 
   tidal_volume_adjusted <- read.csv(paste0(tmpdir, "/tidal_volume_adjusted.csv"), as.is = TRUE, col.names = c("second", "tidal_volume_adjusted")) 
   tidal_volume_raw <- read.csv(paste0(tmpdir, "/tidal_volume_raw.csv"), as.is = TRUE, col.names = c("second", "tidal_volume_raw")) 
  
-  
-  
+  res <- full_join(acceleration_X, acceleration_Y) %>% 
+    full_join(., acceleration_Z) %>% 
+    full_join(., activity) %>% 
+    full_join(., breathing_rate) %>% 
+    full_join(., cadence) %>% 
+    full_join(., device_position) %>% 
+    full_join(., heart_rate) %>% 
+    full_join(., minute_ventilation_adjusted) %>% 
+    full_join(., minute_ventilation_raw) %>% 
+    full_join(., tidal_volume_adjusted) %>% 
+    full_join(., tidal_volume_raw) 
 
-   
+ 
+  res$datetime <- startdate + res$second
+  
+  
+    
 }
 
 
 
-createDateTime <- function(.data, startdate, seconds_column){
+createDateTime <- function(seconds, startdate){
   #seconds_column <- "timesecond"
   #startdate <- 
   .data$datetime <- startdate + .data[[seconds_column]]
@@ -74,10 +91,17 @@ avgToSecond <- function(.data){
   # startdate <- info$start_date
   savenames <- names(.data)
   names(.data) <- letters[1:ncol(.data)]
-  .data <- mutate(.data, timesecond = floor(a)) %>% 
-    group_by(timesecond) %>% summarise(avg = mean(b))
+  .data <- mutate(.data, timesecond = round(a)) 
+  
+  # See Git issue 46, average 0 and 1
+  .data$timesecond[.data$timesecond==0] <- 1
+  
+  
+  .data <- group_by(.data, timesecond) %>% summarise(avg = mean(b))
 
   names(.data) <- savenames
+
+  .data
 }
 
 
